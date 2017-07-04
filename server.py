db34807812816d5e8e55b7f0c0b4c0cf6be2b2bd
Camila Miner <minercamila@gmail.com>
2017-07-04 00:26:04 +0000

    Login function being handled correctly

--- server.py
+++ server.py
@@ -6,12 +6,47 @@ from model import connect_to_db, User, Pet, PetInterest, Adoption, Species
 
 
 app = Flask(__name__)
+app.secret_key = "ABC"
+# app.jinja_env.undefined = StrictUndefined
 
 
-@app.route('/')
-def see_homepage():
+@app.route("/")
+def show_homepage():
 
     return render_template('homepage.html')
+
+
+@app.route("/", methods=["POST"])
+def log_user_in():
+
+    email = request.form.get("email")
+    form_password = request.form.get("password")
+
+    existing_user = User.query.filter(User.email == email).first() 
+
+    if existing_user is None:
+
+        flash("You must create an account first")
+        return redirect("/")
+
+    else:
+
+        user_id = existing_user.user_id
+        user_password = existing_user.password
+
+        if form_password != user_password:
+
+            flash("The password you entered does not match your account")
+            return redirect("/")
+
+        else:
+
+            session["user_id"] = user_id
+
+            flash("You're successfully logged in")
+            return redirect("/")
+
+    #redirect routes are temporary, until new templates are designed
 
 
 @app.route('/puppy')
@@ -21,4 +56,11 @@ def see_puppy():
 
 
 if __name__ == '__main__':
+
+    app.debug = True
+    app.jinja_env.auto_reload = app.debug 
+
+    connect_to_db(app)
+    DebugToolbarExtension(app)
+
     app.run(host="0.0.0.0")
