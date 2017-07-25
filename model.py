1e4ca54f23f81f532cc70cbe51db3d73124911a0
Camila <minercamila@gmail.com>
2017-07-25 23:04:10 +0000

    Working on frontend features

--- model.py
+++ model.py
@@ -192,6 +192,7 @@ def connect_to_db(app):
     # Configure to use our PstgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///barkbook'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
     db.app = app
     db.init_app(app)
 
